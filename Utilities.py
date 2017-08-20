d71887ce8da046a0d08e04a68ed7aa3ebe37ffe6
Python1001 <ggwong16@gmail.com>
2017-08-20 10:52:18 +0800

    Add files via upload

---
+++ Utilities.py
@@ -0,0 +1,108 @@
+﻿#
+#  Created on: Oct 25, 2015
+#  Last modified on: Oct 22, 2016
+#  Author: Changwei Xiong
+#  
+#  Copyright (C) 2015, Changwei Xiong, 
+#  axcw@hotmail.com, <http://www.cs.utah.edu/~cxiong/>
+#
+#  Licence: Revised BSD License
+#
+  
+# from bisect import bisect_left 
+# bisect_left: a[:i] < x <= a[i:] or a[i-1] < x <= a[i]
+# bisect_right: a[:i] <= x < a[i:] or a[i-1] <= x < a[i]
+
+from math import log
+import time
+from functools import wraps
+import numpy as np
+from scipy.interpolate import interp1d 
+from colorama import Fore, Style, init
+#init(autoreset=True)
+
+#import IPython; IPython.embed(); import ipdb; ipdb.set_trace()
+
+class FormatPrint:
+    def __init__(self, format='{0:.2f}'):
+        self.format = format
+        return
+
+    def __call__(self, *args, **kwargs):
+        args = (self.format.format(a) if isinstance(a, float) else a for a in args)
+        kwargs = {k:self.format.format(a) if isinstance(a, float) else a for k,v in kwargs.items()}
+        builtins.print(*args, **kwargs)
+
+
+def convert_to_interpolator(size=300, ns=4.5, kind='linear'):
+    def decorate(fn):
+        @wraps(fn) 
+        def interpolator(x):
+            stdev = np.std(x)
+            if stdev == 0:
+                return fn(np.mean(x))
+            else:
+                xv = np.linspace(-stdev * ns, stdev * ns, size)
+                xv[0] = np.min(x)
+                xv[-1] = np.max(x)
+                yv = [fn(z) for z in xv]             
+                return interp1d(x=xv, y=yv, kind=kind)(x)
+        return interpolator
+    return decorate 
+
+
+def time_this(fn):
+    """
+    decorator that reports the execution time.
+    """
+    # use @wraps(fn) to expose the function name and docstring to the caller of
+    # the decorated function (otherwise, we would see the function name and
+    # docstring for the decorator, not the function it decorates).
+    @wraps(fn) 
+    def wrapper(*args, **kwargs):
+        function_name = Style.BRIGHT + Fore.BLUE + fn.__name__ + Style.RESET_ALL
+        print(function_name + ' starts ...')
+        start = time.time()
+        result = fn(*args, **kwargs)
+        print(function_name + ' finished in ' + '{0:.2f}'.format(time.time() - start), 'seconds\n')
+        return result
+    return wrapper 
+
+
+#def cache_function(order): # similar to functools "@lru_cache(maxsize=None)"
+#    def decorate(fn):
+#        cache = {}
+#        @wraps(fn) 
+#        def wrapper(*args, **kwargs):
+#            try:
+#                return cache[args[order]]
+#            except:
+#                cache[args[order]] = fn(*args, **kwargs)
+#                return cache[args[order]]
+#        return wrapper
+#    return decorate 
+
+def print_self(indent=2, length=8, linespace=0): # similar to functools "@lru_cache(maxsize=None)"
+    def decorate(fn):
+        @wraps(fn) 
+        def wrapper(self):
+            fn(self)
+            d = vars(self)
+            res = (k + ' ' * (length - len(k)) + ': ' + str(d[k]) for k in sorted(d))
+            newline = '\n' + ' ' * indent
+            return newline + newline.join(res) + newline * linespace
+        return wrapper
+    return decorate 
+
+def print_results(pv_pde=None, pv_mc=None):
+    if pv_pde is not None:
+        print('PV(PDE): ' + Style.BRIGHT + Fore.GREEN + '%s' % pv_pde)
+    if pv_mc is not None:
+        print('PV(MC ): ' + Style.BRIGHT + Fore.GREEN + '%s' % pv_mc)
+    if pv_pde is not None and pv_mc is not None:
+        print('diff.% : ' + Style.BRIGHT + Fore.RED + '%s%%' % '{0:.6f}'.format(log(pv_pde / pv_mc) * 100.0) + Style.RESET_ALL)  
+
+if __name__ == '__main__':
+    pass
+
+
